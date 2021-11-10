9c6abf4c8695f881a18e5fcdde5095b63a1f4708
Kim <kimpfimp@posteo.net>
2021-11-10 13:10:41 +0100

    commit

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 namespace Functions;
+
+$db = new Database();
+$request = new Request();
+$response = new Response();
+$filename = "database.json";
+
+error_reporting(-1);
     class Database{
         function open($filename){
             return json_decode(file_get_contents($filename), true);
@@ -49,18 +56,40 @@ namespace Functions;
 
         function getQuery(){
             $query = [];
-            if( str_contains($_SERVER["QUERY_STRING"], "&") ){
+            // TODO: create control that only array queries are allowed arrays as value.
+            if( strpos($_SERVER["QUERY_STRING"], "&") ){
                 $array = explode("&", $_SERVER["QUERY_STRING"]);
                 foreach ( $array as $queryStrings ){
                     $queryArray = explode("=", $queryStrings);
+                    if( strpos($queryArray[1], "," ) ){
+                        $queryArray[1] = explode(",", $queryArray[1]);
+                    }
                     array_push($query, [$queryArray[0]=>$queryArray[1]]);
                 }
             }else{
                 $queryArray = explode("=", $_SERVER["QUERY_STRING"]);
                 array_push($query, [$queryArray[0]=>$queryArray[1]]);
         }
-        return $query;
-    }
+            return $query;
+        }
+
+        function controlValue($query){
+            $param = key($query);
+            $val = $query[$param];
+            switch ($param) {
+                case 'id':
+                    intVal($val) ? true : false;
+                    break;
+                
+                case 'ids':
+                    is_array( $val) ? true : false;
+                    break;
+                
+                case 'limit':
+                    intVal($val) ? true : false;
+                    break;
+            }
+        }        
 }
 
     class Response{
@@ -71,5 +100,24 @@ namespace Functions;
             echo $json;
             exit();
         }
+
+        function queryError($query){
+            $param = key($query);
+            $val = $query[$param];
+            switch ($param) {
+                case 'id':
+                    $this->send( ["error" => "Query 'id' must be of type int"], 400 );
+                    break;
+                
+                case 'ids':
+                    $this->send( ["error" => "Query 'ids' must be of type array and values of type int"], 400 );
+                    break;
+                
+                case 'limit':
+                    $this->send( ["error" => "Query 'limit' must be of type int"], 400 );
+                    break;
+            }
+        }
     }
+
 ?>
--- read.php
+++ read.php
@@ -2,26 +2,31 @@
 header( "Content-type: application/json" );
 require_once "functions.php";
 
-$db = new Functions\Database();
-$request = new Functions\Request();
-$response = new Functions\Response();
-$filename = "database.json";
-
     if( $request->isMethod("GET") ){
         if( $request->isQuery() ){
             $query = $request->getQuery();
-            $response->send($query);
+            if( count($query) === 1 ){
+                $query = $query[0];
+                if ( $request->controlValue($query) ){
+                    $response->send($query);
+                }else{
+                    $response->queryError($query);
+                }
+            }else{
+                $response->send(
+                    ["message" => "Only one query per request allowed"],
+                    400
+                );
+            }
         }else{
             $allUsers = $db->getAll($filename);
             $response->send($allUsers);
         }
     }else{
         $response->send(
-            ["message" => "YOU SUCK!"],
-            418
+            ["message" => "Method not allowed"],
+            405
         );
     }
-
-//$db->find(1, $filename);
 
 ?>
